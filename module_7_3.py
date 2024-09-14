6140a745cb9d9ef20e2eedf4466d553e3abe35d9
wh1tepr1nc3 <amamamania@mail.ru>
2024-09-14 18:25:17 +0500

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,34 @@
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                line = file.read().lower()
+                for punctuation in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    line = line.replace(punctuation, '')
+                words = line.split()
+                all_words[file_name] = words
+                # print(all_words[file_name])
+        return all_words
+
+    def find(self, word):
+        dict_ = {}
+        for name, words in self.get_all_words().items():
+            if word.lower() in words:
+                dict_[name] = words.index(word.lower()) + 1
+        return dict_
+
+    def count(self, word):
+        dict_ = {}
+        for name, words in self.get_all_words().items():
+            dict_[name] = words.count(word.lower())
+            return dict_
+
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
